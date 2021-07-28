5b04f2c783fd5d82af4ae1a76876a73d200dbe90
Jack Ellis <jackellis1504@gmail.com>
2021-07-28 22:52:12 +0100

    Emails?

---
+++ app/Observers/EmailsAdminsObserver.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Observers;
+
+use App\Mail\ModelUpdated;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+
+class EmailsAdminsObserver
+{
+    /**
+     * Handle the "created" event.
+     *
+     * @param  $model
+     * @return void
+     */
+    public function created($model)
+    {
+        Mail::to(User::where('admin', true)->get()->all())->send(new ModelUpdated('created', $model));
+    }
+
+    /**
+     * Handle the Model "updated" event.
+     *
+     * @param  $model
+     * @return void
+     */
+    public function updated($model)
+    {
+        Mail::to(User::where('admin', true)->get()->all())->send(new ModelUpdated('updated', $model));
+    }
+
+    /**
+     * Handle the Model "deleted" event.
+     *
+     * @param  $model
+     * @return void
+     */
+    public function deleted($model)
+    {
+        //
+    }
+
+    /**
+     * Handle the Model "restored" event.
+     *
+     * @param  $model
+     * @return void
+     */
+    public function restored($model)
+    {
+        //
+    }
+
+    /**
+     * Handle the Model "force deleted" event.
+     *
+     * @param  $model
+     * @return void
+     */
+    public function forceDeleted($model)
+    {
+        //
+    }
+}
---
+++ app/Traits/EmailsAdmins.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\Traits;
+
+trait EmailsAdmins {
+    public static function bootEmailsAdmins() {
+        static::observe(EmailsAdminsOberver::class);
+    }
+}
