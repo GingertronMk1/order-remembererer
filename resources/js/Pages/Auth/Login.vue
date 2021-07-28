6cb917bc184291f50740f86fd6054f71174415c2
Jack Ellis <jackellis1504@gmail.com>
2021-07-28 18:55:13 +0100

    Add register link

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -44,7 +44,14 @@
         </label>
       </div>
 
-      <div class="flex items-center justify-end mt-4">
+      <div class="flex items-center justify-end mt-4 space-x-4">
+        <inertia-link
+          :href="route('register')"
+          class="underline text-sm text-gray-600 hover:text-gray-900"
+        >
+            Register
+        </inertia-link>
+
         <inertia-link
           v-if="canResetPassword"
           :href="route('password.request')"
