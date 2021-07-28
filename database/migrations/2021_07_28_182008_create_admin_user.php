75bed2e927cec88d8670004032b727f6546aa22e
Jack Ellis <jackellis1504@gmail.com>
2021-07-28 19:35:16 +0100

    Update user and add admin

---
+++ database/migrations/2021_07_28_182008_create_admin_user.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $user = new User();
+        $user->name = 'ADMIN';
+        $user->email = 'jackellis1504+order-remembererer@gmail.com';
+        $user->password = bcrypt('P4ssWord!');
+        $user->email_verified_at = now();
+        $user->admin = true;
+        $user->save();
+
+        $team = new Team();
+        $team->name = 'TEAM ADMIN';
+        $team->user_id = $user->id;
+        $team->personal_team = false;
+        $team->save();
+        $team->users()->attach($user);
+
+        $user->current_team_id = $team->id;
+        $user->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
