812b9132d6a267936e0fe21fac872ffa0bd478c2
Jack Ellis <jackellis1504@gmail.com>
2021-07-28 22:18:21 +0100

    Add some globally useful components

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ require("./bootstrap");
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Card from "@/Components/Card.vue";
 
 const appName =
   window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
@@ -13,6 +15,8 @@ createInertiaApp({
   setup({ el, app, props, plugin }) {
     return createApp({ render: () => h(app, props) })
       .use(plugin)
+      .component("app-layout", AppLayout)
+      .component("card", Card)
       .mixin({ methods: { route } })
       .mount(el);
   },
