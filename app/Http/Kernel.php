b4b33ada6a5ddbb3780032833b87b46ac3877d25
Jack Ellis <jackellis1504@gmail.com>
2021-07-28 18:50:54 +0100

    Updated team middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,11 +58,11 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'ensure-team' => \App\Http\Middleware\EnsureTeam::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'ensure-team' => \App\Http\Middleware\EnsureTeam::class,
     ];
 }
