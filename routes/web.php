fc5c21543d1bb00c310e6b9c59e76e8cb42ae773
Jack Ellis <jackellis1504@gmail.com>
2021-07-30 22:23:48 +0100

    Update middleware reqs

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
 Route::middleware([
     'auth:sanctum',
     'verified',
     'ensure-team',
 ])->group(function () {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
     Route::resources([
         'cuisine' => CuisineController::class,
         'vendor' => VendorController::class,
