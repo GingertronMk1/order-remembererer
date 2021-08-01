7c7d69a349b6fc316ac9fccc61d9ecaf1d7580f7
Jack Ellis <jackellis1504@gmail.com>
2021-08-01 22:07:08 +0100

    Moved back to migration

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Team;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,24 +11,5 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        if (0 === User::count()) {
-            $user = new User();
-            $user->name = 'ADMIN';
-            $user->email = 'jackellis1504+order-remembererer@gmail.com';
-            $user->password = bcrypt('P4ssWord!');
-            $user->email_verified_at = now();
-            $user->admin = true;
-            $user->save();
-
-            $team = new Team();
-            $team->name = 'TEAM ADMIN';
-            $team->user_id = $user->id;
-            $team->personal_team = false;
-            $team->save();
-            $team->users()->attach($user);
-
-            $user->current_team_id = $team->id;
-            $user->save();
-        }
     }
 }
