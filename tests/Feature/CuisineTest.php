b6a46f8d5eb5a136f945bf7fd6405fd6c6aba3dc
Jack Ellis <jackellis1504@gmail.com>
2021-08-02 08:20:40 +0100

    Fixed

--- tests/Feature/CuisineTest.php
+++ tests/Feature/CuisineTest.php
@@ -20,40 +20,40 @@ class CuisineTest extends TestCase
         $cuisine = Cuisine::factory()->create();
 
         $tests = [
-            "index" => function() use ($cuisine) {
+            'index' => function () {
                 return $this->get(route('cuisine.index'));
             },
-            "create" => function() use ($cuisine) {
+            'create' => function () {
                 return $this->get(route('cuisine.create'));
             },
-            "store" => function() use ($cuisine) {
+            'store' => function () {
                 return $this->post(route('cuisine.store'), []);
             },
-            "show" => function() use ($cuisine) {
+            'show' => function () use ($cuisine) {
                 return $this->get(route('cuisine.show', compact('cuisine')));
             },
-            "edit" => function() use ($cuisine) {
+            'edit' => function () use ($cuisine) {
                 return $this->get(route('cuisine.edit', compact('cuisine')));
             },
-            "update" => function() use ($cuisine) {
+            'update' => function () use ($cuisine) {
                 return $this->put(route('cuisine.update', compact('cuisine')), []);
             },
-            "destroy" => function() use ($cuisine) {
+            'destroy' => function () use ($cuisine) {
                 return $this->delete(route('cuisine.destroy', compact('cuisine')));
             },
         ];
 
-        foreach($tests as $test) {
+        foreach ($tests as $test) {
             $response = $test();
-            echo $response->getContent() . PHP_EOL . '---' . PHP_EOL;
+            echo $response->getContent().PHP_EOL.'---'.PHP_EOL;
             $response->assertRedirect(route('login'));
         }
 
         $user = User::factory()->withPersonalTeam()->create();
-        echo print_r($user, true) . PHP_EOL . '---' . PHP_EOL;
+        echo print_r($user, true).PHP_EOL.'---'.PHP_EOL;
         $this->actingAs($user);
 
-        foreach($tests as $test) {
+        foreach ($tests as $test) {
             $response = $test();
             $response->assertStatus(200);
         }
