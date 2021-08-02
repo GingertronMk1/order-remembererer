0fa27d00d2b18c8a25b87c7a8e05f454db7254c5
Jack Ellis <jackellis1504@gmail.com>
2021-08-02 21:46:58 +0100

    Fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -74,7 +74,8 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->hasMany(Cuisine::class);
     }
 
-    public function vendors() {
+    public function vendors()
+    {
         return $this->hasMany(Vendor::class);
     }
 }
