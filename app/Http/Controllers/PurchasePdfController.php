c08473b595ff659f4238986eb189ca5c37012f84
Jack Ellis <jackellis1504@gmail.com>
2021-08-03 23:49:05 +0100

    Get timezone

--- app/Http/Controllers/PurchasePdfController.php
+++ app/Http/Controllers/PurchasePdfController.php
@@ -15,24 +15,29 @@ class PurchasePdfController extends Controller
      */
     public function __invoke(Purchase $purchase, Request $request)
     {
-        $title = "Order for {$purchase->vendor->name} at {$purchase->expires_at}";
-        $view = view('pdf.purchase', compact('purchase', 'title'));
-        // reference the Dompdf namespace
-        if (0) {
-            return $view;
+        if ($request->get('timezone')) {
+            $expiry = $purchase->expires_at->setTimezone($request->get('timezone'));
+            $title = "Order for {$purchase->vendor->name} at {$expiry}";
+            $view = view('pdf.purchase', compact('purchase', 'title'));
+            // reference the Dompdf namespace
+            if (0) {
+                return $view;
+            }
+
+            // instantiate and use the dompdf class
+            $dompdf = new Dompdf();
+            $dompdf->loadHtml($view);
+
+            // (Optional) Setup the paper size and orientation
+            $dompdf->setPaper('A4', 'portrait');
+
+            // Render the HTML as PDF
+            $dompdf->render();
+
+            // Output the generated PDF to Browser
+            return $dompdf->stream($title.'.pdf', ['Attachment' => false]);
         }
 
-        // instantiate and use the dompdf class
-        $dompdf = new Dompdf();
-        $dompdf->loadHtml($view);
-
-        // (Optional) Setup the paper size and orientation
-        $dompdf->setPaper('A4', 'portrait');
-
-        // Render the HTML as PDF
-        $dompdf->render();
-
-        // Output the generated PDF to Browser
-        $dompdf->stream($title.'.pdf', ['Attachment' => false]);
+        return inertia('PurchasePdf/Redirect', ['purchase-id' => $purchase->id]);
     }
 }
